cbd71f8ab7849fca5d3f12a6c8927d25330e8d4d
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-09 18:43:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/' do
+    erb :user_input
   end
+  
   post '/piglatinize' do
     @piglatin = PigLatinizer.new(params[:user_text])
     erb :output
   end
+  
 end
