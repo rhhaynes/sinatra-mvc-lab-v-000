4bc2d83ae43abd916dd2b49506df81ece93177d2
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-09 20:42:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,11 +15,15 @@ class PigLatinizer
     vow = %w[a e i o u]
     con = ("a".."z").to_a - vow
     
-    while con.include?(str[0].downcase)
-      str += str[0]
-      str = str[1..-1]
+    if vow.include?(str[0].downcase)
+      str + "way"
+    else
+      while con.include?(str[0].downcase)
+        str += str[0]
+        str = str[1..-1]
+      end
+      str + "ay"
     end
-    str + 'ay'
   end
   
 end
