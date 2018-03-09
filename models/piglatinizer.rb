1f7909c4b3bf16692efaa9b3cee7f41f400467fc
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-09 20:08:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,18 +6,24 @@ class Piglatinizer
   end
   
   def piglatinize
+    text.split(" ").collect{|word| translate(word)}.join(" ")
+  end
+  
+  private
+  
+  def translate(str)
     vow = %w[a e i o u]
     con = ("a".."z").to_a - vow
-
-  if vowels.include?(str[0])
-    str + 'ay'
-  elsif consonants.include?(str[0]) && consonants.include?(str[1])
-    str[2..-1] + str[0..1] + 'ay'
-  elsif consonants.include?(str[0])
-    str[1..-1] + str[0] + 'ay'
-  else
-    str # return unchanged
-  end
+    
+    if vow.include?(str[0])
+      str + 'ay'
+    elsif consonants.include?(str[0]) && consonants.include?(str[1])
+      str[2..-1] + str[0..1] + 'ay'
+    elsif consonants.include?(str[0])
+      str[1..-1] + str[0] + 'ay'
+    else
+      str # return unchanged
+    end
   end
   
 end
